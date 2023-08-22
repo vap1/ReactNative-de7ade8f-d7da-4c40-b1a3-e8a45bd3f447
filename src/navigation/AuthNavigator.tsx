bac31cc3defb91f0ed5a7de85b86a00e67fcecac
vap1 <vaibhavvasant1705@gmail.com>
2023-08-22 15:20:31 +0530

    First Commit

---
+++ src/navigation/AuthNavigator.tsx
@@ -0,0 +1,30 @@
+
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+import LoginScreen from '../screens/LoginScreen';
+import RegisterScreen from '../screens/RegisterScreen';
+
+const Stack = createStackNavigator();
+
+const AuthNavigator = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Register"
+          component={RegisterScreen}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default AuthNavigator;
